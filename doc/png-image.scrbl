f83a79d2eac86c885b9e64c4f72c081803030719
Lehi Toskin <lehi@tosk.in>
2016-12-13 19:06:32 -0800

    Complete zTXt and tEXt

--- doc/png-image.scrbl
+++ doc/png-image.scrbl
@@ -21,7 +21,7 @@
   the name of a chunk (which is itself a hash). If a chunk may appear multiple
   times according to the PNG spec, then the chunk information is wrapped inside
   a list of hashes. In txt chunks, if the text has been deflated, it will be
-  undeflated in this step.
+  inflated in this step.
 
   The chunk hashes have the keys @racket['(type data length crc)].
 }
@@ -30,39 +30,55 @@
   Given a hash, returns a valid PNG image as a byte string. If converted from
   one form to the other, the resulting byte string may not be identical to the
   original bytes due to the ordering of the chunks, but all the chunks should
-  inside the final result. If the hash has txt chunks, the text data will be
-  deflated if it exceeds 1024 bytes.
+  be inside the final result. If the hash has iTXt chunks, the text data will
+  be deflated if it exceeds 1024 bytes.
 }
 
 @defmodule[png-image/txt]{
   @racketmodname[png-image/txt] provides the following procedures.
-
+   
+  tEXt data hashes have the keys @racket['(keyword text)].
+  
+  zTXt data hashes have the keys @racket['(keyword compression-method text)].
+  
   iTXt data hashes have the keys @racket['(keyword
                                            compression-flag
-                                           compression-mode
+                                           compression-method
                                            language-tag
                                            translated-keyword
                                            text)].
+  
+  The only valid compression-method value is @racket[#"\0"].
 }
 
-@defproc[(itxt-data->hash (bstr bytes?)) hash?]{
-  Take an incomplete iTXt chunk that contains only the data and create a hash.
-  If the data is longer than 1024 bytes, it will NOT be deflated here.
+@deftogether[(@defproc[(text-data->hash (bstr bytes?)) hash?]
+              @defproc[(ztxt-data->hash (bstr bytes?)) hash?]
+              @defproc[(itxt-data->hash (bstr bytes?)) hash?])]{
+  Take an incomplete chunk that contains only the data and create a hash.
+  In iTXt, if the data is longer than 1024 bytes, it will NOT be deflated here.
 }
 
-@defproc[(itxt-hash->data (hsh hash?)) bytes?]{
-  Take an iTXt chunk hash and return iTXt data.
+@deftogether[(@defproc[(text-hash->data (hsh hash?)) bytes?]
+              @defproc[(ztxt-hash->data (hsh hash?)) bytes?]
+              @defproc[(itxt-hash->data (hsh hash?)) bytes?])]{
+  Take an incomplete chunk hash and return incomplete data.
 }
 
-@defproc[(make-itxt-chunk (keyword string?)
+@deftogether[(@defproc[(make-text-chunk (keyword string?) (str string? "")) bytes?]
+              @defproc[(make-ztxt-chunk (keyword string?) (str string? "")) bytes?]
+              @defproc[(make-itxt-chunk (keyword string?)
                           (str string? "")
                           (language-tag string? "")
-                          (translated-kw string? "")) bytes?]{
-  Create a complete iTXt chunk with the keyword. If @racket[str] exceeds 1024
-  bytes, it will be deflated.
+                          (translated-kw string? "")) bytes?])]{
+  Create a complete chunk with the keyword. In iTXt, if @racket[str] exceeds
+  1024 bytes, it will be deflated. In zTXt, @racket[str] will always be
+  deflated.
 }
 
-@defproc[(make-itxt-hash (chunk bytes?)) hash?]{
-  Create a complete iTXt hash from the complete @racket[chunk] bytes. If the
-  text from @racket[chunk] is deflated, it will be inflated in this step.
+@deftogether[(@defproc[(make-text-hash (chunk bytes?)) hash?]
+              @defproc[(make-ztxt-hash (chunk bytes?)) hash?]
+              @defproc[(make-itxt-hash (chunk bytes?)) hash?])]{
+  Create a complete hash from the complete @racket[chunk] bytes. In iTXt, if
+  the text from @racket[chunk] is deflated, it will be inflated in this step.
+  In zTXt, the text will always be deflated.
 }
--- txt.rkt
+++ txt.rkt
@@ -12,7 +12,16 @@
          itxt-set
          make-itxt-chunk
          make-itxt-hash
-         ztxt-data->hash)
+         make-text-chunk
+         make-text-hash
+         make-ztxt-chunk
+         make-ztxt-hash
+         text-data->hash
+         text-hash->data
+         text-set
+         ztxt-data->hash
+         ztxt-hash->data
+         ztxt-set)
 
 ; tEXt:
 ; LENGTH
@@ -51,7 +60,7 @@
 ;                   ; to a hash via (read-itxt-chunk)
 ;                   'data (hash 'keyword keyword
 ;                               'compression-flag cflag
-;                               'compression-mode cmode
+;                               'compression-method cmethod
 ;                               'language-tag ltag
 ;                               'translated-keyword tkw
 ;                               'text bstr)
@@ -69,6 +78,31 @@
        (bitwise-xor (quotient accum 2)
                     (* #xEDB88320 (bitwise-and accum 1)))))
    #xFFFFFFFF))
+
+; reads a tEXt bye string and returns the data in a hash
+(define/contract (text-data->hash bstr)
+  (bytes? . -> . hash?)
+  (define bstr-in (open-input-bytes bstr))
+  (define type (peek-bytes 4 4 bstr-in))
+
+  (define kw
+    ; loop until we find \0
+    ; skip length and type
+    (let loop ([offset 8])
+      (define byte (peek-bytes 1 offset bstr-in))
+      (if (bytes=? byte #"\0")
+          (peek-bytes (- offset 8) 8 bstr-in)
+          (loop (add1 offset)))))
+  (define kw-len (bytes-length kw))
+
+  (define data (subbytes bstr
+                         (+ 10 kw-len)
+                         (- (bytes-length bstr) 4)))
+
+  (close-input-port bstr-in)
+
+  (hash 'keyword kw
+        'text data))
 
 ; reads a zTXt byte string and returns the uncompressed data in a hash
 (define/contract (ztxt-data->hash bstr)
@@ -178,6 +212,33 @@
           uncompressed]
          [else data])))
 
+; take the text-data hash and returns a tEXt byte string
+(define/contract (text-hash->data hsh)
+  (hash? . -> . bytes?)
+  (define inner (hash-ref hsh 'data))
+  (bytes-append (hash-ref inner 'keyword)
+                #"\0"
+                (hash-ref inner 'text)))
+
+; take the ztxt-data hash and returns a zTXt byte string
+; - will be compressed!
+(define/contract (ztxt-hash->data hsh)
+  (hash? . -> . bytes?)
+  (define inner (hash-ref hsh 'data))
+  ; deflate the text data
+  (bytes-append (hash-ref inner 'keyword)
+                #"\0"
+                (hash-ref hsh 'compression-method)
+                (cond [(bytes=? (hash-ref hsh 'compression-method) #"\0")
+                       (define uncompressed-in (open-input-bytes (hash-ref inner 'text)))
+                       (define uncompressed-out (open-output-bytes))
+                       (deflate uncompressed-in uncompressed-out)
+                       (define compressed (get-output-bytes uncompressed-out))
+                       (close-input-port uncompressed-in)
+                       (close-output-port uncompressed-out)
+                       compressed]
+                      [else (hash-ref inner 'text)])))
+
 ; take the itxt-data hash and returns an iTXt byte string
 (define/contract (itxt-hash->data hsh)
   (hash? . -> . bytes?)
@@ -192,7 +253,59 @@
                 #"\0"
                 (hash-ref inner 'text)))
 
-; reads a string and returns an iTXt chunk byte string with compressed(?) data
+; reads strings and returns a tEXt chunk byte string
+; creates a complete tEXt chunk
+(define/contract (make-text-chunk keyword [str ""])
+  (->* (string?)
+       (string?)
+       bytes?)
+  (define type #"tEXt")
+  (define kw (string->bytes/latin-1 keyword))
+  (define bstr (string->bytes/latin-1 str))
+  (define info+data
+    (bytes-append kw
+                  #"\0"
+                  bstr))
+  (with-output-to-bytes
+      (λ ()
+        (printf "~a~a~a~a"
+                (number->bytes (bytes-length info+data))
+                type
+                info+data
+                (number->bytes (bytes-crc32 info+data))))))
+
+; reads strings and returns a zTXt chunk byte string with compressed data
+; creates a complete zTXt chunk
+(define/contract (make-ztxt-chunk keyword [str ""])
+  (->* (string?)
+       (string?)
+       bytes?)
+  (define type #"zTXt")
+  (define kw (string->bytes/latin-1 keyword))
+  (define bstr (string->bytes/latin-1 str))
+  (define compression-method (bytes 0))
+  (define data
+    (let ([data-in (open-input-bytes bstr)]
+          [data-out (open-output-bytes)])
+      (deflate data-in data-out)
+      (define compressed (get-output-bytes data-out))
+      (close-input-port data-in)
+      (close-output-port data-out)
+      compressed))
+  (define info+data
+    (bytes-append kw
+                  #"\0"
+                  compression-method
+                  data))
+  (with-output-to-bytes
+      (λ ()
+        (printf "~a~a~a~a"
+                (number->bytes (bytes-length info+data))
+                type
+                info+data
+                (number->bytes (bytes-crc32 info+data))))))
+
+; reads strings and returns an iTXt chunk byte string with compressed(?) data
 ; creates a complete iTXt chunk
 (define/contract (make-itxt-chunk keyword [str ""] [language-tag ""] [translated-kw ""])
   (->* (string?)
@@ -237,6 +350,28 @@
                   (number->bytes (bytes-crc32 info+data))))))
   itxt-bstr)
 
+; reads a complete tEXt chunk byte string and returns a hash of the chunk
+; with a nested hash for the data
+; creates a complete tEXt hash
+(define/contract (make-text-hash chunk)
+  (bytes? . -> . hash?)
+  (define len (bytes->number (subbytes chunk 0 4)))
+  (hash 'type #"tEXt"
+        'data (text-data->hash chunk)
+        'length len
+        'crc32 (subbytes chunk (- (bytes-length chunk) 4))))
+
+; reads a complete zTXt chunk byte string and returns a hash of the chunk
+; with a nested hash for the compressed zTXt data
+; creates a complete zTXt hash
+(define/contract (make-ztxt-hash chunk)
+  (bytes? . -> . hash?)
+  (define len (bytes->number (subbytes chunk 0 4)))
+  (hash 'type #"zTXt"
+        'data (ztxt-data->hash chunk)
+        'length len
+        'crc32 (subbytes chunk (- (bytes-length chunk) 4))))
+
 ; reads a complete iTXt chunk byte string and returns a hash of the chunk
 ; with a nested hash for the compressed(?) iTXt data
 ; creates a complete iTXt hash
@@ -247,6 +382,36 @@
         'data (itxt-data->hash chunk)
         'length len
         'crc32 (subbytes chunk (- (bytes-length chunk) 4))))
+
+; takes a PNG hash and a tEXt hash
+; returns a new PNG hash with the tEXt hash added
+(define/contract (text-set png-hash text-hash keyword)
+  (hash? hash? string? . -> . hash?)
+  (cond [(hash-has-key? png-hash 'tEXt)
+         (define kw-bstr (string->bytes/latin-1 keyword))
+         (define text-lst
+           (for/list ([text (in-list (hash-ref png-hash 'tEXt))])
+             (define text-data-hash (hash-ref text 'data))
+             (if (bytes=? kw-bstr (hash-ref text-data-hash 'keyword))
+                 text-hash
+                 text)))
+         (hash-set png-hash 'tEXt text-lst)]
+        [else (hash-set png-hash 'tEXt (list text-hash))]))
+
+; takes a PNG hash and a zTXt hash
+; returns a new PNG hash with the iTXt hash added
+(define/contract (ztxt-set png-hash ztxt-hash keyword)
+  (hash? hash? string? . -> . hash?)
+  (cond [(hash-has-key? png-hash 'zTXt)
+         (define kw-bstr (string->bytes/latin-1 keyword))
+         (define ztxt-lst
+           (for/list ([ztxt (in-list (hash-ref png-hash 'zTXt))])
+             (define ztxt-data-hash (hash-ref ztxt 'data))
+             (if (bytes=? kw-bstr (hash-ref ztxt-data-hash 'keyword))
+                 ztxt-hash
+                 ztxt)))
+         (hash-set png-hash 'zTXt ztxt-lst)]
+        [else (hash-set png-hash 'zTXt (list ztxt-hash))]))
 
 ; takes a PNG hash and an iTXt hash
 ; returns a new PNG hash with the iTXt hash added
